988b8ee3162b3eb48ec90686fdb33901a2be9ba6
antoniolopez1 <antoniolopez0063@gmail.com>
2020-02-09 23:07:10 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'learning-angular';
+  persona = {
+  	nombre: 'Antonio',
+  	edad: 21
+  }
+
 }
